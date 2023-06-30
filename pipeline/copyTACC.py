045f742161277dfff60883728338b6735f3a11e1
Akhil Sadam <sadam.akhil@gmail.com>
2023-06-30 09:57:45 -0500

    update

--- pipeline/copyTACC.py
+++ pipeline/copyTACC.py
@@ -11,3 +11,4 @@ _dir = userparam.param['work_dir']
 # copy back all .png files using rsync from user@ls6.tacc.utexas.edu
 os.makedirs('../model_ls6/', exist_ok=True)
 os.system(f'rsync -zarv --include="*/" --include="*.png" --exclude="*" {user}@ls6.tacc.utexas.edu:{_dir}/model_ls6/* ../model_ls6/')
+os.system(f'rsync -zarv --include="*/" --include="*.png" --exclude="*" {user}@ls6.tacc.utexas.edu:{_dir}/data/* ../data_ls6/')
