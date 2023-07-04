41edcd8311e411125a7d86cb50c75df34b2b6b39
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-04 13:20:45 -0500

    upd

--- pipeline/user/as_tacc_param.py
+++ pipeline/user/as_tacc_param.py
@@ -1,7 +1,7 @@
 param=\
 {# installation directory, working directory, and data directories, respectively
     'install_dir': r'WeatherPrediction/ERA5_PredRNN/',
-    'work_dir': r'/work/07752/as_tacc/ls6/WeatherPrediction/ERA5_PredRNN/',
+    'work_dir': r'../',
     'data_dir': r'../data',
     'model_dir': r'../model_ls6',
     'WSL': False,
