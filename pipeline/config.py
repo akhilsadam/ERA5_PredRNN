82f09fe7342bbc4b0533fab9c5cbbb8236c84b72
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-06 18:13:07 -0500

    more updates for weather data

--- pipeline/config.py
+++ pipeline/config.py
@@ -27,6 +27,44 @@ model_config = \
             'activation': 'relu', # activation function
             'optimizer' :  lambda x,y : ASGD(x,lr=y) # [None, Adam, ASGD,...]'
         },
+        'BERT_POD':{
+            'n_layers': 4, # number of layers in the transformer
+            'n_head': 1, # number of heads in the transformer
+            'n_embd': 100, # number of hidden units in the transformer
+            'n_ffn_embd': 100, # number of hidden units in the FFN
+            'dropout': 0.1, # dropout rate
+            'initialization': None, # initialization method as list of functions
+            'activation': 'relu', # activation function
+            'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
+            'scheduler' : lambda x : CyclicLR(x, base_lr=1e-5, max_lr=5e-4, cycle_momentum=False, step_size_up=20),
+            'batch_size': 16, # batch size
+        },
+        'reZeroTF_POD':{
+            'n_layers': 4, # number of layers in the transformer
+            'n_head': 1, # number of heads in the transformer
+            'n_embd': 100, # number of hidden units in the transformer
+            'n_ffn_embd': 100, # number of hidden units in the FFN
+            'dropout': 0.1, # dropout rate
+            'initialization': None, # initialization method as list of functions
+            'activation': 'relu', # activation function
+            'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
+            'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=2e-4, cycle_momentum=False, step_size_up=20),
+            'batch_size': 16, # batch size
+        },        
+        'LSTM_POD':{
+            'n_layers': 4, # number of layers 
+            'n_embd': 100, # number of hidden units
+            'dropout': 0.1, # dropout rate
+            'optimizer' :  lambda x,y : Adam(x, lr=5e-4), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
+            'scheduler' : lambda x : CyclicLR(x, base_lr=1e-5, max_lr=5e-4, cycle_momentum=False, step_size_up=20),
+            'batch_size': 16, # batch size
+        },
+        'predrnn_v2_POD':{
+            "optimizer": None, # uses default Adam as configured below
+            'batch_size': 16, # batch size
+            'test_batch_size': 16, # batch size for testing -- for some reason this needs to be the same as batch_size
+            'patch_size': 1, # divides the image l,w - breaks it into patches that are FCN into the hidden layers (so each patch_size x patch_size -> # of hidden units).
+        }
 
     }
 model_config_toy = \
