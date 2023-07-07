1520b6474717da4b730054d4e4d62479d75c0224
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-06 21:19:51 -0500

    upd

--- pipeline/config.py
+++ pipeline/config.py
@@ -284,7 +284,7 @@ preprocessor_config = \
         'POD':{
             'eigenvector': lambda var: f'POD_eigenvector_{var}.npz', # place to store precomputed eigenvectors in the data directory
             # (var is the variable name)
-            'make_eigenvector': False, # whether to compute eigenvectors or not (only needs to be done once)
+            'make_eigenvector': True, # whether to compute eigenvectors or not (only needs to be done once)
             'max_n_eigenvectors': 100, # ballpark number of eigenvectors (otherwise uses PVE to determine)
             'PVE_threshold': 0.999, # PVE threshold to determine number of eigenvectors
         },
