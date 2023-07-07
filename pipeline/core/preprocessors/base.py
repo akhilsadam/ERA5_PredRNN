f55610df3b56b5f1c50af40bdddb298571069401
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-07 11:36:38 -0500

    config upd

--- pipeline/core/preprocessors/base.py
+++ pipeline/core/preprocessors/base.py
@@ -14,9 +14,11 @@ class PreprocessorBase:
         assert self.shapex > 0, "shapex (x dimension of data) must be specified and greater than 0"
         assert self.shapey > 0, "shapey (y dimension of data) must be specified and greater than 0"             
 
-        self.scale_path =  f"{self.datadir}/scale.txt"
-        
         self.weather_prediction = config['weather_prediction']
+        wp = '_WP' if self.weather_prediction else ''
+        self.scale_path =  f"{self.datadir}/scale{wp}.txt"
+        
+        
         
     def precompute_check(self):
         # check if scale file exists
