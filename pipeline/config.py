a089406ffaa396382de44a064da43efc020635ac
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-18 23:34:28 -0500

    POD4

--- pipeline/config.py
+++ pipeline/config.py
@@ -68,6 +68,33 @@ model_config = \
             'batch_size': 2, # batch size
             'test_batch_size': 2, # batch size for testin
         },        
+        'DualAttentionTransformer':{
+            'windows': [[16,16],[2,2],[2,2]], # list of window sizes for the shifted attention
+            'shifts': [[0,0],[0,0],[1,1]], # list of shifts for the shifted attention
+            'n_head': 1, # number of heads in the transformer
+            'n_embd': 4096, # number of hidden units in the transformer
+            'n_ffn_embd': 4096, # number of hidden units in the FFN
+            'dropout': 0.1, # dropout rate
+            'initialization': None, # initialization method as list of functions
+            'activation': 'relu', # activation function
+            'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
+            'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=2e-4, cycle_momentum=False, step_size_up=20),
+            'batch_size': 2, # batch size
+            'test_batch_size': 2, # batch size for testin
+        },
+        'BERT':{
+            'n_layers': 4, # number of layers in the transformer
+            'n_head': 1, # number of heads in the transformer
+            'n_embd': 4096, # number of hidden units in the transformer
+            'n_ffn_embd': 4096, # number of hidden units in the FFN
+            'dropout': 0.1, # dropout rate
+            'initialization': None, # initialization method as list of functions
+            'activation': 'relu', # activation function
+            'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
+            'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=2e-4, cycle_momentum=False, step_size_up=20),
+            'batch_size': 2, # batch size
+            'test_batch_size': 2, # batch size for testin
+        },
         'LSTM_POD':{
             'n_layers': 4, # number of layers 
             'n_embd': 100, # number of hidden units
