d991fdc43e8f0a614c33b050886cca43bc2d0fba
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-27 02:01:48 -0500

    add rezerotf NAT

--- pipeline/config.py
+++ pipeline/config.py
@@ -422,7 +422,7 @@ preprocessor_config = \
         'POD_v4':{
             'eigenvector': lambda var: f'POD_v4_eigenvector_{var}.npz', # place to store precomputed eigenvectors in the data directory
             # (var is the variable name)
-            'make_eigenvector': False, # whether to compute eigenvectors or not (only needs to be done once)
+            'make_eigenvector': True, # whether to compute eigenvectors or not (only needs to be done once)
             'max_n_eigenvectors': 400, # ballpark number of eigenvectors (otherwise uses PVE to determine)
             'PVE_threshold': 0.99999, # PVE threshold to determine number of eigenvectors
             'n_patch': 1, # x,y patch number (so 8x8 of patches = full image)
