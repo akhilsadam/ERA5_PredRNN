a84963170ae0cc6101f5a244786f1a5a5b47868f
Akhil Sadam <sadam.akhil@gmail.com>
2023-07-30 17:22:12 -0500

    wp update

--- pipeline/core/models/RZTX_NAT_LG.py
+++ pipeline/core/models/RZTX_NAT_LG.py
@@ -134,7 +134,7 @@ class PositionalEncoding(nn.Module):
 class NAT(nn.Module):
     def __init__(self, d, num_heads, k1, k2, channels, spatial, device) -> None:
         super().__init__()
-        # assert k1%2==1 and k2%2==1, "Kernel sizes must be odd!"
+        assert k1%2==1 and k2%2==1, "Kernel sizes must be odd!"
         # standard conv layer, then atrous conv layer such that the entire region is covered
         ###
         # Modules expect inputs of shape [batch_size, *, dim]:
@@ -317,7 +317,7 @@ class RZTXEncoderLayer(Module):
         spatial = reduced_shape is not None        
         self.reduced_shape = reduced_shape
         if not spatial: channels = 1
-        self.conv = NAT(seq_len, nhead, 32, 32, channels,spatial=spatial, device=device)
+        self.conv = NAT(seq_len, nhead, 33, 33, channels,spatial=spatial, device=device)
         
         self.dropout1 = Dropout(dropout)
         self.dropout2 = Dropout(dropout)
