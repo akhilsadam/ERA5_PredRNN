bba884aaf265b71efd901311dd0753fe4bad6a3d
Akhil Sadam <sadam.akhil@gmail.com>
2023-08-02 13:06:05 -0500

    update

--- pipeline/config.py
+++ pipeline/config.py
@@ -52,8 +52,8 @@ model_config = \
             'activation': 'relu', # activation function
             'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
             'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=5e-4, cycle_momentum=False, step_size_up=20),
-            'batch_size': 2, # batch size
-            'test_batch_size': 2, # batch size for testin
+            'batch_size': 1, # batch size
+            'test_batch_size': 1, # batch size for testin
         }, 
         'reZeroNAT_POD_v4':{
             'n_layers': 4, # number of layers in the transformer
@@ -65,8 +65,8 @@ model_config = \
             'activation': 'relu', # activation function
             'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
             'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=5e-4, cycle_momentum=False, step_size_up=20),
-            'batch_size': 2, # batch size
-            'test_batch_size': 2, # batch size for testin
+            'batch_size': 1, # batch size
+            'test_batch_size': 1, # batch size for testin
         }, 
         'reZeroCNN_CNN':{
             'n_layers': 4, # number of layers in the transformer
@@ -78,8 +78,8 @@ model_config = \
             'activation': 'relu', # activation function
             'optimizer' :  lambda x,y : Adam(x, lr=5e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
             'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=2e-4, cycle_momentum=False, step_size_up=20),
-            'batch_size': 2, # batch size
-            'test_batch_size': 2, # batch size for testin
+            'batch_size': 1, # batch size
+            'test_batch_size': 1, # batch size for testin
         }, 
         'reZeroNAT_LG':{
             'n_layers': 1, # number of layers in the transformer # memory issues with increasing this
