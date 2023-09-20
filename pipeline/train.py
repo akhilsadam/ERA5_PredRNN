142225ca3aa8e32e9697864c15b5c6b43755abb3
Akhil Sadam <sadam.akhil@gmail.com>
2023-09-20 01:13:11 -0500

    reduce number of output prints

--- pipeline/train.py
+++ pipeline/train.py
@@ -59,7 +59,7 @@ hyp = hyperparam()
 # hyp.overrides.update({'n_ffn_embd': 200}) #128
 # hyp.overrides.update({'n_head': 4})
 hyp.n_valid = 12 if hyp.weather_prediction else 1
-hyp.max_iterations = 401
+hyp.max_iterations = 1001
 # hyp.overrides.update({'n_embd': 400}) #64
 if mode == -1:
     tr = [False]
