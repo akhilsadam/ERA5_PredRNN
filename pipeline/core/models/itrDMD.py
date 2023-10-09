5756b3d33f57ed555cbd01533302f444b70315fa
GodotMisogi <GodotMisogi@github.com>
2023-10-09 16:26:59 -0500

    Iterative DMD kinda not working

--- pipeline/core/models/itrDMD.py
+++ pipeline/core/models/itrDMD.py
@@ -3,6 +3,10 @@ import torch
 import torch.nn as nn
 from core.models.model_base import BaseModel
 from core.loss import loss_mixed
+import logging
+from pydmd import DMD
+
+logger = logging.getLogger("hate")
   
 class DMDIntegrator(BaseModel):
 
@@ -25,6 +29,7 @@ class DMDIntegrator(BaseModel):
         self.input_length = configs.input_length
         self.predict_length = configs.total_length - configs.input_length
         self.total_length = configs.total_length
+        self.dmd = DMD(svd_rank=20)
 
         self.resweight = nn.Parameter(torch.Tensor([0]))
 
@@ -39,36 +44,50 @@ class DMDIntegrator(BaseModel):
         dim_b, dim_t, dim_c, dim_x, dim_y = seq_total.shape
 
         # 1-channel implementation
-        out = torch.zeros((dim_b, self.total_length, dim_x, dim_y)) # Indexed by T
-        out[:,:self.input_length,:,:] = seq_total[:,:self.input_length,0,:,:] # BTCHW
+        out = torch.zeros((dim_b, self.total_length, dim_x, dim_y)).to(self.device).to(torch.cfloat) # Indexed by T
+        out[:,:self.input_length,:,:] = seq_total[:,:self.input_length,0,:,:].to(self.device).to(torch.cfloat) # BTCHW
         
         # Iterate over batches
-        for i in range(out.shape[1]):
+        for i in range(out.shape[0]):
             # Iterate over timesteps
-            for j in range(self.input_length, self.predict_length - 1):
-                print(f"Batch: {i}, Time index: {j}")
+            for j in range(self.input_length, self.total_length):
+
+                logger.info(f"Batch: {i}, Time index: {j}")
+                
                 # Get current batch up to current time
-                data = torch.reshape(out[i,j-self.input_length:j,:,:], (j, dim_x * dim_y)) # Reshape into (nt, nx * ny) matrix for DMD
+                data = torch.reshape(out[i,:j,:,:], (j, dim_x * dim_y)) # Reshape into (nt, nx * ny) matrix for DMD
+
+                logger.info(f"Reshaped array of length {j}.")
 
                 # Do the DMD, shake your DMD
-                self.dmd.fit(data)
+                U, S, Vh = torch.linalg.svd(data[:-1,:].T, full_matrices=True)
+                Y = data[1:,:].T
+
+                S_pinv = torch.cat((torch.diag(1. / S), torch.zeros(U.shape[1] - Vh.shape[0], Vh.shape[0]).to(self.device).to(torch.cfloat)), dim=0).T
+
+                logger.info(f"SVD completed. Sizes {(U.H.shape, Y.shape, Vh.H.shape, S_pinv.shape)}")
 
                 # Generate the integration matrix
                 A = torch.linalg.multi_dot(
-                        [modes, torch.diag(self.dmd.eigs), torch.linalg.pinv(self.dmd.modes)]
-                    ).to(device).to(torch.cfloat)
+                        [U.H, Y, Vh.H, S_pinv]
+                    ).to(self.device).to(torch.cfloat)
+
+                logger.info("Integration matrix generated.")
 
                 # Integrate current state to get new state
-                inpt = torch.reshape(out[i,j,:,:], (dim_x * dim_y))
+                inpt = torch.reshape(out[i,j-1,:,:], (dim_x * dim_y,1))
                 new_st = (A @ inpt).T # Integrated new state
+
+                logger.info("State integrated.")
+
+                # print(new_st)
  
                 # Assign new state to next timestep of current batch
-                out[i,j+1,:,:] = reshape(new_st, (dim_x, dim_y))
+                out[i,j,:,:] = torch.reshape(new_st, (dim_x, dim_y)).T
 
 
         # Reshape
-        # outpt = torch.real(torch.cat(outpt, dim=1)) # BT(W*H)
-        out = out.reshape((dim_b, self.total_length, dim_c, dim_x, dim_y))
+        out = torch.real(out.reshape((dim_b, self.total_length, dim_c, dim_x, dim_y)))
 
         loss_pred = self.resweight
         loss_decouple = torch.tensor(0.0)
