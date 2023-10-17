c166eb1d19c9483c66960c9fedafda08399ba269
Akhil Sadam <sadam.akhil@gmail.com>
2023-10-17 00:34:28 -0500

    weather

--- pipeline/config.py
+++ pipeline/config.py
@@ -122,16 +122,16 @@ model_config = \
             'test_batch_size': 1, # batch size for testin
         },        
         'reZeroTF_POD_snapshot':{
-            'n_layers': 6, # number of layers in the transformer
+            'n_layers': 8, # number of layers in the transformer
             'n_head': 2, # number of heads in the transformer
-            'n_embd': 200, # number of hidden units in the transformer
-            'n_ffn_embd': 200, # number of hidden units in the FFN
+            'n_embd': 400, # number of hidden units in the transformer
+            'n_ffn_embd': 400, # number of hidden units in the FFN
             'dropout': 0.1, # dropout rate
             'initialization': None, # initialization method as list of functions
             'activation': 'relu', # activation function
             'optimizer' :  lambda x,y : Adam(x, lr=1e-5), # final_lr=0.1), #SGD(x, lr=0.4),#, momentum=0.1, nesterov=True), #ASGD(x,lr=100*y), # [None, Adam, ASGD,...]'
             'scheduler' : lambda x : CyclicLR(x, base_lr=5e-6, max_lr=5e-5, cycle_momentum=False, step_size_up=20),
-            'batch_size': 2, # batch size
+            'batch_size': 1, # batch size
             'test_batch_size':1,
         },
         'reZeroTF_POD_v4':{
